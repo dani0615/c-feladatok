8b5ffcc4e2d2e3a8391a545e14f736854f6b369f
haluskad <haluskad@201-14>
2025-02-10 09:30:10 +0100

    ora vege

--- FileOpenSaveApplicationWPF/FileOpenSaveApplicationWPF/MainWindow.xaml.cs
+++ FileOpenSaveApplicationWPF/FileOpenSaveApplicationWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using FileOpenSaveApplicationWPF.Models;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace FileOpenSaveApplicationWPF
 {
@@ -16,11 +19,77 @@ namespace FileOpenSaveApplicationWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        static List<Auto> autok = new List<Auto>();
+
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        public void Beolvas(string filename)
+        {
+            try
+            {
+                foreach (string sor in File.ReadLines(filename).Skip(1).ToArray())
+                {
+                    autok.Add(new Auto(sor));
+                }
+            }
+            catch (FileNotFoundException fnfex)
+            {
+                MessageBox.Show("A fájl nem található: \n" + fnfex.Message);
+            }
+            catch (FormatException fex)
+            {
+                MessageBox.Show("Hibás formátum: \n" + fex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            
+            
 
         }
 
+        private void Beolvasas_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialogq = new OpenFileDialog();
+            openFileDialogq.Filter = "Szöveges állományok |*.txt";
+
+            if (openFileDialogq.ShowDialog().Value)
+            {
+                  Beolvas(openFileDialogq.FileName);
+                  lbAutok.Items.Clear();
+                  lbAutok.ItemsSource = autok;
+
+            }
+            else
+                MessageBox.Show("Nincw kiválasztott fájl!");
+
+        }
+
+        private void lbAutok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(lbAutok.SelectedItem!= null)
+            {
+                Auto kivalasztott = (Auto)lbAutok.SelectedItem;
+                tbxAzonosito.Text = kivalasztott.Id.ToString();
+                tbxGyartmany.Text = kivalasztott.Marka;
+                tbxTipus.Text = kivalasztott.Tipus;
+                tbxSzin.Text = kivalasztott.Szin;
+                tbxEvjarat.Text = kivalasztott.Evjarat.ToString();
+                tbxMuszaki.Text = kivalasztott.Muszaki.ToString();
+                ckxAktiv.IsChecked = kivalasztott.Aktiv;
+
+
+            }
+            else
+                MessageBox.Show("Nincs kiválasztott elem!");
+
+
+        }
     }
 }
--- FileOpenSaveApplicationWPF/FileOpenSaveApplicationWPF/Models/Auto.cs
+++ FileOpenSaveApplicationWPF/FileOpenSaveApplicationWPF/Models/Auto.cs
@@ -13,7 +13,27 @@ namespace FileOpenSaveApplicationWPF.Models
         public string Tipus { get; set; }
         public string Szin { get; set; }
         public int Evjarat { get; set; }
-
         public DateOnly Muszaki { get; set; }
+        public bool Aktiv { get; set; }
+        
+       
+
+
+        public Auto (string sor)
+        {
+            string[] adatok = sor.Split(';');
+            Id = int.Parse(adatok[0]);
+            Marka = adatok[1];
+            Tipus = adatok[2];
+            Szin = adatok[3];
+            Evjarat = int.Parse(adatok[4]);
+            Muszaki = DateOnly.Parse(adatok[5]);
+            Aktiv = bool.Parse(adatok[6]);
+           
+        }
+        public override string ToString()
+        {
+            return $"{Id}: ; {Marka}: ; {Tipus}: ";
+        }
     }
 }
