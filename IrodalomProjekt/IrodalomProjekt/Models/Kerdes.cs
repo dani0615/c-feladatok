25de4b718fa7647b34e2c953d61617c7e11122c2
haluskad <haluskad@201-14>
2025-02-17 09:02:01 +0100

    konstruktor valtozas

--- IrodalomProjekt/IrodalomProjekt/Models/Kerdes.cs
+++ IrodalomProjekt/IrodalomProjekt/Models/Kerdes.cs
@@ -8,22 +8,21 @@ namespace IrodalomProjekt.Models
 {
     class Kerdes
     {
-        public Kerdes(string kerdesSzovege, string valaszA, string valaszB, string valaszC, string helyesValasz, string felhvalasz)
+        public Kerdes(string kerdesSzovege, string valaszA, string valaszB, string valaszC, string helyesValasz)
         {
             KerdesSzovege = kerdesSzovege;
             ValaszA = valaszA;
             ValaszB = valaszB;
             ValaszC = valaszC;
             HelyesValasz = helyesValasz;
-            FelhValasz = felhvalasz;
         }
 
-        public string KerdesSzovege { get; set; }
+        public string KerdesSzovege { get; set; } 
         public string ValaszA { get; set; }
         public string ValaszB { get; set; }
         public string ValaszC { get; set; }
         public string HelyesValasz { get; set; }
-        public string FelhValasz { get; set; }
+        public string? FelhValasz { get; set; }
 
     }
 }
