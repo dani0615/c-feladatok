451b8186a3f6dee804bfc9f7365f29d0e37de108
haluskad <haluskad@201-12>
2025-03-03 10:12:40 +0100

    Lepes ellenőrzése ( foglalt mezőt nem lehet már felülírni)

--- AmobaWPFAlap/AmobaWPFAlap/MainWindow.xaml.cs
+++ AmobaWPFAlap/AmobaWPFAlap/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace AmobaWPFAlap
 {
@@ -81,6 +82,11 @@ namespace AmobaWPFAlap
         {
             int SorSzam = int.Parse((sender as Button).Name.Split("_")[1]);
             int OszlopSzam = int.Parse((sender as Button).Name.Split("_")[2]);
+            if (jatekter[SorSzam, OszlopSzam] != " ")
+            {
+                MessageBox.Show("Ez a mező már foglalt!");
+                return;
+            }
             jatekter[SorSzam, OszlopSzam] = jatekos;
             JatekterKiir();
             if (jatekos == "X")
@@ -91,8 +97,39 @@ namespace AmobaWPFAlap
             {
                 jatekos = "X";
             }
+
+            
         }
 
-        //hf: commitban
+        private void Mentes_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog saveFileDialog = new OpenFileDialog();
+            saveFileDialog.Filter = "Szöveges fájl|*.txt";
+
+
+        }
+
+        private void Kilepes_Click(object sender, RoutedEventArgs e)
+        {
+           MessageBoxResult Yes_or_No= MessageBox.Show("Kiszeretnél lépni a programból?", "Megerősítés", MessageBoxButton.YesNo, MessageBoxImage.Question );
+            if (Yes_or_No == MessageBoxResult.Yes)
+                Application.Current.Shutdown();
+
+        }
+
+        private void Megnyitas_click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Szöveges fájl|*.txt";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                
+                JatekterKiir();
+            }
+
+        }
+
+        //HF: győzelem ellenőrzése , mentés (szöveges állományba  játéktér , következő játékos , játék állása , ) , korábbi játék betöltése a szöveges állományból (OpenFileDialog)
+        //hf : mezők ellenőrzése van-e már X vagy O a mezőben ne lehessen felülírni
     }
 }
