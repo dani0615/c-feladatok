869fe705f661ca7a2a5a6851e3e1649ee1f07b9a
haluskad <haluskad@201-12>
2025-03-03 07:40:49 +0100

    új projekt amoba wpf jatek

---
+++ AmobaWPFAlap/AmobaWPFAlap/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AmobaWPFAlap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string[,]? jatekter;
+        public static string jatekos = "X";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UjJatek(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void JatekterKiir()
+        {
+
+        }
+
+        private void Lepes(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
