680fcd82520ec80c2652e21b034afbfbc95a95e5
dani h <haluskad@kkszki.hu>
2025-03-10 08:24:09 +0100

    Dolgozat 03.10

---
+++ SzamozottGombok/SzamozottGombok/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SzamozottGombok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEllenoriz_Click(object sender, RoutedEventArgs e)
+        {
+            bool fut;
+            int szam;
+            bool eredmeny = int.TryParse(tbxSzam.Text, out szam);
+            if (szam<1 || szam >10)
+            {
+                MessageBox.Show("Határon kívüli szám");
+                fut = false;
+            }
+            if (!eredmeny)
+            {
+                MessageBox.Show("Nem megfelelő számformátum!");
+                fut = false;
+            }
+           
+
+            
+            
+                for (int i = 0; i < szam; i++)
+                {
+                    Button button = new Button()
+                    {
+                        Content = i + 1,
+                        Width = 50,
+                        Height = 30,
+                        Margin = new Thickness(5, 20, 5, 20),
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        
+                        
+                    };
+                    gombok.Children.Add(button);
+                }
+            }
+           
+            
+        }
+    }
