85d7096908b4e1d4f83ffcf52d2c998c59002e42
haluskad <haluskad@201-14>
2025-03-31 09:24:01 +0200

    LinqDoga , alap muveletek + WPF ablak

---
+++ LinqDoga/LinqDoga/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace LinqDoga
+{
+    public class Program
+    {
+        public static List<Video> videok = new List<Video>();
+        static void Main(string[] args)
+        {
+            LoadFromFile("ytlist.txt");
+
+            double atlag = videok.Average(x => x.Megtekintes);
+            double max = videok.Max(x => x.Megtekintes);
+            double min = videok.Min(x => x.Megtekintes);
+            string videocim = videok.Where(x=> x.Eloado == "Robin Schulz").First().Cim;
+
+            Console.WriteLine("A videok megtekintésének átlaga: " + (int)atlag);
+            Console.WriteLine($"A legtöbbször megtekintett videót :{max} alkalommal nézték meg.");
+            Console.WriteLine($"A legkevesebbszer megtekintett videót:{min} alkalommal nézték meg.");
+            Console.WriteLine("A Robin Schulztól szereplő videó: " + videocim);
+
+            
+                    
+            foreach (Video video in videok)
+            {   if(videok.Where(x=> x.Idotartam.StartsWith("2")).ToList().Count > 0)
+                {
+                    Console.WriteLine(video.Idotartam);
+                }
+            else
+                {
+                    Console.WriteLine("Nincs ilyen video");
+                }
+            }
+        }
+
+       public  static List<Video> LoadFromFile(string filename)
+            {
+            var videok = new List<Video>();
+            string[] sorok = File.ReadAllLines("ytlist.txt").Skip(1).ToArray();
+            foreach (string sor in sorok)
+            {
+                videok.Add(new Video(sor));
+            }
+            return videok;
+
+
+        }
+    }
+}
---
+++ LinqDoga/LinqDoga/Video.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDoga
+{
+    public class Video
+    {
+        public int Helyezes { get; set; }
+        public string Idotartam { get; set; }
+        public string Eloado { get; set; }
+        public string Cim { get; set; }
+        public string Kiado { get; set; }
+        public int Megtekintes { get; set; }
+
+       public Video(string sor) 
+        {
+            string[] adatok = sor.Split(';');
+            Helyezes = int.Parse(adatok[0]);
+            Idotartam = (adatok[1]);
+            Eloado = adatok[2];
+            Cim = adatok[3];
+            Kiado = adatok[4];
+            Megtekintes = int.Parse(adatok[5]);
+
+        }
+    }
+}
