e740ab850933855daa0b853ca88f0c5e58b74ff1
haluskad <haluskad@201-14>
2025-04-14 09:38:11 +0200

    Új WPF projekt létrehozása hajó adatkezeléssel
    - Adatfájl hozzáadása: `adatok.txt` (hajó adatok)
    - `App.xaml` és `App.xaml.cs` létrehozása (alkalmazás erőforrások és logika)
    - `AssemblyInfo.cs` hozzáadása (assembly szintű attribútumok)
    - `Hajok` osztály létrehozása: `Hajok.cs` (hajó adatok reprezentálása)
    - `MainWindow.xaml` és `MainWindow.xaml.cs` létrehozása (főablak elrendezés és logika)

---
+++ Felelet04_14/Felelet04_14/Hajok.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Felelet04_14
+{
+    public class Hajok
+    {
+
+        public string Osztaly { get; set; }
+        public string Tipus { get; set; }
+        public string Orszag { get; set; }
+        public int AgyukSzama { get; set; }
+        public int Kaliber { get; set; }
+        public int Vizkiszoritas { get; set; }
+
+
+        public Hajok(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            Osztaly = adatok[0];
+            Tipus = adatok[1];
+            Orszag = adatok[2];
+            AgyukSzama = int.Parse(adatok[3]);
+            Kaliber = int.Parse(adatok[4]);
+            Vizkiszoritas = int.Parse(adatok[5]);
+        }
+
+    }
+
+}
